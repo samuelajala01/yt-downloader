c207db494eecce4df123dfdc7b86ea87a520fbe5
Samuel Ajala <samuelajala01@gmail.com>
2023-02-06 01:31:39 +0000

    init

---
+++ main.py
@@ -0,0 +1,34 @@
+from pytube import YouTube
+
+
+# select_resolution= str(input("Options:\n1. 144p\n2. 240p\n3. 360p\n4. 480p\n5. 720p\n6. 1080p\nEnter preferred resolution: "))
+
+# conditional to select resolution
+
+# if select_resolution == "1" or select_resolution == "144p":
+#     resolution = "144p"
+# elif select_resolution == "2" or select_resolution == "240p":
+#     resolution =  "240p" 
+# elif select_resolution == "3" or select_resolution == "360p":
+#     resolution = "360p"
+# elif select_resolution == "4" or select_resolution == "480p":
+#     resolution = "480p"
+# elif select_resolution == "5" or select_resolution == "720p":
+#     resolution = "720p"
+# elif select_resolution == "6" or select_resolution == "1080p":
+#     resolution = "1080p"          
+
+
+def Download(link):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution
+    try:
+        youtubeObject.download() # add path to download folder
+    except:
+        print("An Error occured")  
+    print("Download successful")  
+
+link = input("Enter youtube video URL: ")
+Download(link)
+
+"https://www.youtube.com/watch?v=jhFDyDgMVUI"
